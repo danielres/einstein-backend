2a7f34109382c9e91abdc48d852cf9afb77a9eec
Daniel Reszka <dannn.r@gmail.com>
2015-06-17 16:33:35 +0200

    Change User test to follow the affirmative wording style
    * follows the "spec" style
      (produces test execution output that reads like documentation)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 describe User do
   let(:user) { User.new(username: 'a', password: '12345678', email: 'test@test.com') }
 
-  it "must be valid" do
+  it "is valid" do
     user.must_be :valid?
   end
 end
