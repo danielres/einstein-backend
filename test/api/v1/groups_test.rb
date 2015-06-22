545fc5674c3c444d4cd8a106a588a4c4b40b6bdc
Daniel Reszka <dannn.r@gmail.com>
2015-06-22 17:28:29 +0200

    Add API test for POST /groups (red)

---
+++ test/api/v1/groups_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class APITest::GroupsTest < ActiveSupport::TestCase
+
+    def app
+      V1::Groups
+    end
+
+    describe "POST /api/groups" do
+      it "creates a group" do
+        post '/api/groups', { name: 'My group' }, 'Content-Type' => 'application/json'
+        last_response.status.must_equal 201
+        expected = {'name' => 'My group' }.to_json
+        last_response.body.must_equal expected
+      end
+    end
+
+  end
