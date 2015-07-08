209d27de4e57ad80d55d97522b7342315424047a
Daniel Reszka <dannn.r@gmail.com>
2015-07-08 15:51:51 +0200

    Add support to token-based authentication in SessionsController#create
    * There are now 2 methods to login:
      1) using a username + password
      2) with a valid access token
    
    Both methods return the same output: a JSON representation of the
    current user with fields "username", "email", "access_token", ...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,30 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user_from_token!
 
-  # POST /v1/login
   def create
-    @user = User.find_for_database_authentication(email: params[:username])
+    attempt_token_authorization
+
+    @user ||= User.find_for_database_authentication(email: params[:username])
     return invalid_login_attempt unless @user
 
-    if @user.valid_password?(params[:password])
+    if current_user
+      render json: @user, serializer: SessionSerializer, root: nil
+    elsif @user.valid_password?(params[:password])
       sign_in :user, @user
       render json: @user, serializer: SessionSerializer, root: nil
     else
       invalid_login_attempt
     end
-  end
+
+    end
 
   private
+
+  def attempt_token_authorization
+    auth_token = request.headers['Authorization']
+    authenticate_with_auth_token(auth_token) if auth_token
+    @user = current_user if current_user
+  end
 
   def invalid_login_attempt
     warden.custom_failure!
