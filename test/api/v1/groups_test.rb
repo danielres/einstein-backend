92fa1b67f705da3ac55b1140c40bac720d7c4ab7
Daniel Reszka <dannn.r@gmail.com>
2015-07-09 18:44:57 +0200

    Add API test for GET '/api/v1/groups' (red)

--- test/api/v1/groups_test.rb
+++ test/api/v1/groups_test.rb
@@ -32,4 +32,26 @@ class APITest::GroupsTest < ActiveSupport::TestCase
 
     end
 
+
+    describe "GET /api/v1/groups" do
+      describe "when groups have been created" do
+        before do
+          post '/api/v1/groups', { group: { name: 'My group 1', description: 'My description 1'} }
+          post '/api/v1/groups', { group: { name: 'My group 2', description: 'My description 2'} }
+        end
+        it "returns an HTTP status 201 with the representation of the resources created" do
+          get '/api/v1/groups'
+          last_response.status.must_equal 200
+          JSON.parse(last_response.body).size.must_equal 2
+          JSON.parse(last_response.body)[0]['id'].must_be :present?
+          JSON.parse(last_response.body)[0]['name'].must_equal 'My group 1'
+          JSON.parse(last_response.body)[0]['description'].must_equal 'My description 1'
+          JSON.parse(last_response.body)[1]['id'].must_be :present?
+          JSON.parse(last_response.body)[1]['name'].must_equal 'My group 2'
+          JSON.parse(last_response.body)[1]['description'].must_equal 'My description 2'
+        end
+      end
+    end
+
+
   end
