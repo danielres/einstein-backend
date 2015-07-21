b1e17ece455317a0ac22174404585711e2b7522c
Daniel Reszka <dannn.r@gmail.com>
2015-07-21 13:29:14 +0200

    Require token-based authentication on every API call
    * Requires every API call to provide a valid access token
      in order to access the API
    
    * Introduces helper "current_user" to use inside the grape API
    
    * Introduces method User#find_by_authentication_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,22 @@ class User < ActiveRecord::Base
     save
   end
 
+  def self.find_by_authentication_token(auth_token)
+    unless auth_token.include?(':')
+      raise "authentication_error"
+      return
+    end
+
+    user_id = auth_token.split(':').first
+    user = User.where(id: user_id).first
+
+    if user && Devise.secure_compare(user.access_token, auth_token)
+      user
+    else
+      raise "authentication_error"
+    end
+  end
+
+
+
 end
