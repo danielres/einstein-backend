63d62baeb024726551c49bca5e3684c7e795b318
Daniel Reszka <dannn.r@gmail.com>
2015-07-23 18:44:40 +0200

    Add model "DiscussionEntry" for AddingADiscussionEntry (green)
    * Completes "ef40aba Add API/integration test for AddingADiscussionEntry"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723152153) do
+ActiveRecord::Schema.define(version: 20150723163802) do
+
+  create_table "discussion_entries", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "discussion_id"
+    t.integer  "author_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "discussions", force: :cascade do |t|
     t.string   "title"
